08376c2ab23d60546999721b551bb8762d4691ab
ankit <ankit.kishorelv@gmail.com>
2022-11-15 17:52:23 +0530

    wrote a logic for reading the data from file purchase details

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -8,9 +8,28 @@ import java.text.SimpleDateFormat;
 
 public class SalesDataAnalyzer {
 
-
+    int count=0;
+    int index=0;
     // This method reads a file and adds each line of the file into the corresponding SalesRecord object
     public SalesRecord[] readFile(String fileName) {
+        try(FileReader reader=new FileReader(fileName);
+        BufferedReader buffer=new BufferedReader(reader)){
+        String line=buffer.readLine();
+        while ((line= buffer.readLine())!=null);
+            { count++;}
+
+        }catch(IOException exception){
+            exception.getMessage();
+        }
+//for reading the single line using instance variable.
+        SalesRecord[] salesRecords=new SalesRecord[count];
+        try( FileReader fileReader=new FileReader(fileName);
+             BufferedReader buffer=new BufferedReader(fileReader)) {
+
+            String line=buffer.readLine();
+
+
+        }catch(IOException exception){}
         return null;
     }
 
